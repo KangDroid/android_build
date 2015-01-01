8a3e2fdd388553ee5af479e819ccabfb98d8233c
KangDroid <KangDroid@KangDroidui-MacBook-Air.local>
2015-01-01 11:36:14 +0000

    Add my clean step This is a test commit so that this could be erased or reset.

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -305,6 +305,9 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 # 5.0.2
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
+# Clean my ChangeLog:KangDroid-Project
+$(call add-clean-step, rm -rf vendor/kdp/utils/KangDroid-Project_ChangeLog.txt)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
